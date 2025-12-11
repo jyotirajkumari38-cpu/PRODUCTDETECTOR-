1a99f5b5eb5e4a354a853bad1f24f321875d718e
jyotirajkumari38-cpu <jyotirajkumari38@gmail.com>
2025-12-11 20:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -454,7 +454,7 @@ def render_amazon_dashboard():
 
 def render_stock_predictor():
     """Module B: Advanced Stock Predictor with Random Forest"""
-    st.title("📈 Pro Stock Analytics Suite")
+    st.title("📈 Pro Stock jyoti  Analytics Suite")
     st.markdown("Real-time data fetching, technical indicators, and Random Forest prediction.")
     
     # 1. Sidebar Controls for this module
